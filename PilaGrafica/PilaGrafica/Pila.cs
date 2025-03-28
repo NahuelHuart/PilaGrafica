63645940032fb7c97d17352918f69f7d5b6ed03e
NahuelHuart <a2409833@institutmontilivi.cat>
2025-03-28 09:06:23 +0100

    Form amb pila i taulallista

---
+++ PilaGrafica/PilaGrafica/Pila.cs
@@ -0,0 +1,232 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PILA
+{
+    public class Pila<T> : ICollection<T>
+    {
+        const int DEFAULT_SIZE = 5;
+        private T[] data;
+        private int top;
+
+        public Pila()
+        {
+            data = new T[DEFAULT_SIZE];
+            top = -1;
+        }
+        public Pila(int capacity)
+        {
+            top = -1;
+            data = new T[capacity];
+        }
+
+        public Pila(IEnumerable<T> items)
+        {
+            if(items == null || items.Count() == 0)
+            {
+                throw new ArgumentException("La colección no puede ser nula o vacía");
+            }
+
+            data = new T[DEFAULT_SIZE];
+            top = -1;   
+            foreach (T item in items)
+            {
+                if (this.IsFull) this.EnsureCapacity(this.Count*2);
+                this.Push(item);
+            }
+        }
+
+        public T this[int index]
+        {
+            get
+            {
+                if (index < 0 || index > top)
+                    throw new ArgumentOutOfRangeException();
+                return data[index];
+            }
+        }
+
+        public int Count
+        {
+            get
+            {
+                return top + 1;
+            }
+        }
+
+        public bool IsReadOnly
+        {
+            get
+            {
+                return false;
+            }
+        }
+
+        public void Add(T item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Clear()
+        {
+            //for (int i = 0; i < Count; i++)
+            //    data[i] = default;
+
+            data = new T[data.Length];
+
+            top = -1;
+        }
+
+        public bool Contains(T item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void CopyTo(T[] array, int arrayIndex)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            return new EnumeradorPila<T>(data, top);
+        }
+
+        public bool Remove(T item)
+        {
+            throw new NotImplementedException();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return new EnumeradorPila<T>(data, top);
+        }
+
+        public bool IsFull
+        {
+            get
+            {
+                return top == Count;
+            }
+        }
+
+        public bool IsEmpty
+        {
+            get
+            {
+                return top == -1;
+            }
+        }
+
+        public int Capacity
+        {
+            get
+            {
+                return this.data.Length;
+            }
+        }
+        public T Pop()
+        {
+            if (IsEmpty)
+                throw new InvalidOperationException("La pila está vacía");
+            T item = data[top];
+            data[top] = default;
+            top--;
+            return item;
+        }
+        public T Peek()
+        {
+            if (IsEmpty)
+                throw new InvalidOperationException("La pila está vacía");
+            return data[top];
+        }
+        public void Push(T item)
+        {
+            if (IsFull)
+                throw new InvalidOperationException("La pila está llena");
+            
+            data[++top] = item;
+        }
+
+        public T[] ToArray()
+        {
+            T[] result = new T[Count];
+            for (int i = 0; i < Count; i++)
+                result[i] = data[i];
+            return result;
+        }
+        public int EnsureCapacity(int newCapacity)
+        {
+            //mirar si la nova capacitat és major que l'actual
+            if (this.Capacity < newCapacity)
+            {
+                //si la nova capacitat és major
+
+                //crear un nou array amb la nova capacitat
+                T[] data2 = new T[newCapacity];
+
+                //copiar les dades al array
+                for (int i = 0; i < this.Count; i++)
+                    data2[i] = this.data[i];
+
+                //assignar el nou array a la variable data de la pila
+                this.data = data2;
+            }
+
+            //retorna la capacitat actual de la pila
+            return this.Capacity;
+        }
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.Append("[");
+
+            foreach (T item in this)
+            {
+                sb.Append(item).Append(",");
+                
+            }
+
+            if(!this.IsEmpty) sb.Remove(sb.Length - 1, 1);
+
+            sb.Append("]");
+
+            return sb.ToString();
+        }
+
+        public bool Equals(object obj)
+        {
+            bool iguals = true;
+
+            if (obj == null || !(obj is Pila<T>))
+            {
+                iguals = false; 
+            } 
+
+            Pila<T> pila = (Pila<T>)obj;
+            
+            if (this.Count != pila.Count)
+            {
+                iguals = false;
+            }
+
+            int i = 0;
+            while (iguals && i < this.Count)
+            {
+
+                iguals = this.data[i].Equals(pila.data[i]);
+
+                i++;
+            }   
+
+
+            return iguals;
+
+        }
+
+    }
+}
---
+++ PilaGrafica/PilaGrafica/TaulaLlista.cs
@@ -0,0 +1,223 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EQUIPS
+{
+    class TaulaLlista<T> : IList<T>
+    {
+        const int LONGITUD_INICIAL = 10;
+        private T[] dades;
+        private int nElem;
+
+
+
+        public TaulaLlista()
+        {
+            nElem = 0;
+            dades = new T[LONGITUD_INICIAL];
+        }
+
+        private void DoubleCapacity()
+        {
+            T[] dades2;
+            dades2 = new T[dades.Length * 2];
+
+            for (int i = 0; i < dades.Length; i++)
+            {
+                dades2[i] = dades[i];
+            }
+            dades = dades2;
+        }
+        public int IndexOf(T item)
+        {
+            int index = 0;
+            bool trobat = false;
+            while (!trobat && index < nElem)
+            {
+                if (dades[index].Equals(item))
+                    trobat = true;
+                else
+                    index++;
+            }
+
+            if (!trobat) index = -1;
+            return index;
+        }
+
+        public void Add(T item)
+        {
+            if (IsReadOnly) throw new NotSupportedException();
+
+            if (nElem == dades.Length)
+            {
+                DoubleCapacity();
+            }
+
+            dades[nElem] = item;
+
+            nElem++;
+        }
+
+        public void Clear()
+        {
+            /*T[] dades2;
+            dades2 = new T[dades.Length];
+
+            for (int i = 0; i < nElem; i++)
+            {
+                dades[i] = dades2[i];
+            }*/
+
+            dades = new T[LONGITUD_INICIAL];
+            nElem = 0;
+        }
+
+        public bool Contains(T item)
+        {
+            bool trobat = false;
+
+            if (IndexOf(item) != -1)
+                trobat = true;
+
+            return trobat;
+        }
+
+        public void CopyTo(T[] array, int arrayIndex)
+        {
+            for (int i = 0; i < this.Count; i++)
+            {
+                array[arrayIndex + i] = this[i];
+            }
+        }
+
+        public bool Remove(T item)
+        {
+            if (this.IsReadOnly) throw new NotSupportedException();
+
+            bool trobat = false;
+            int pos = 0;
+            while (!trobat)
+            {
+                if (item.Equals(dades[pos]))
+                {
+                    for (int i = pos; i < nElem; i++)
+                    {
+                        dades[pos] = dades[pos + 1];
+                    }
+
+                    trobat = true;
+                }
+                else
+                    pos++;
+            }
+            if (trobat) nElem--;
+            return trobat;
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            for(int i = 0; i<nElem; i++)
+            {
+                yield return dades[i];
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+
+        public void Insert(int index, T item)
+        {
+            if (index < 0 || index >= nElem)
+            {
+                throw new ArgumentOutOfRangeException(nameof(index), "L'índex està fora dels límits.");
+            }
+
+            if (nElem == dades.Length)
+            {
+                DoubleCapacity();
+            }
+
+            for (int i = nElem; i > index; i--)
+            {
+                dades[i] = dades[i - 1];
+            }
+
+            dades[index] = item;
+            nElem++;
+        }
+
+        public void RemoveAt(int index)
+        {
+            if (IsReadOnly)
+                throw new NotSupportedException();
+
+            if (index < 0 || index >= nElem)
+            {
+                throw new ArgumentOutOfRangeException(nameof(index), "L'índex està fora dels límits.");
+            }
+
+            for (int i = index; i < nElem - 1; i++)
+            {
+                dades[i] = dades[i + 1];
+            }
+
+            nElem--;
+            dades[nElem] = default; // no em deixa fer dades[nElem] = null; llavors faig això perque un objecte T no pot ser null sempre perque es generic.
+        }
+
+
+        public int Count
+        {
+            get
+            {
+                return nElem;
+            }
+        }
+
+        public bool IsReadOnly
+        {
+            get
+            {
+                return false;
+            }
+        }
+
+        public T this[int index]
+        {
+            get
+            {
+                if (index < 0 || index >= nElem)
+                    throw new ArgumentOutOfRangeException();
+                return dades[index];
+            }
+
+            set => dades[index] = value;
+        }
+
+        public void AddRange(IEnumerable<T> items)
+        {
+            /*foreach (T item in items)
+            {
+             this.Add(item);
+            }*/
+            IEnumerator<T> iterador = (IEnumerator<T>)items.GetEnumerator();
+
+            while (iterador.MoveNext())
+                this.Add(iterador.Current);
+        }
+        public void Sort()
+        {
+            Array.Sort(dades, 0, nElem);
+        }
+        public void Sort(IComparer<T> comparer)
+        {
+            Array.Sort(dades, 0, nElem, comparer);
+        }
+    }
+}
